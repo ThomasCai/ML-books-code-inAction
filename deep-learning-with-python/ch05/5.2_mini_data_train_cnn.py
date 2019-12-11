114f6c74b36721a3a07c429dfba752c9855af7dc
thomas_cai <295727207@qq.com>
2019-12-12 00:12:36 +0800

    create 5.2

---
+++ deep-learning-with-python/ch05/5.2_mini_data_train_cnn.py
@@ -0,0 +1,232 @@
+import os, shutil
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing import image  # 图像预处理工具模块
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+create_data_dir = True  # 创建文件夹，只需运行一次，注意路径
+the_first_exp = True  # 将猫狗分类的小型卷积神经网络实例化
+data_improve_example = True
+the_second_exp = False # 定义一个包含 dropout 的新卷积神经网络 and include data improve
+
+# 原始数据集解压目录的路径
+original_dataset_dir = '/Users/fchollet/Downloads/kaggle_original_data'
+# 保存较小数据集的目录
+base_dir = '/Users/fchollet/Downloads/cats_and_dogs_small'
+os.mkdir(base_dir)
+# 分别对应划分后的训练、验证和测试的目录
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+# 创建猫和狗的 训练、验证、训练的目录
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+
+if create_data_dir:
+    # 将前1000张猫的图像复制到 train_cats_dir
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(train_cats_dir, fname)
+        shutil.copyfile(src, dst)
+    # 将接下来500张猫的图像复制到 validation_cats_dir
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(validation_cats_dir, fname)
+        shutil.copyfile(src, dst)
+    # 将接下来500张猫的图像复制到 test_cats_dir
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(test_cats_dir, fname)
+        shutil.copyfile(src, dst)
+    # 将前1000张狗的图像复制到 train_dogs_dir
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(train_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+    # 将前500张狗的图像复制到 validation_dogs_dir
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(validation_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+    # 将前500张狗的图像复制到 test_dogs_dir
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(test_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+    print('total training cat images:', len(os.listdir(train_cats_dir)))
+    print('total training dog images:', len(os.listdir(train_dogs_dir)))
+    print('total validation cat images:', len(os.listdir(validation_cats_dir)))
+    print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+    print('total test cat images:', len(os.listdir(test_cats_dir)))
+    print('total test dog images:', len(os.listdir(test_dogs_dir)))
+
+
+if the_first_exp:
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    print(model.summary())
+
+    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+    # 使用 ImageDataGenerator 从目录中读取图像
+    train_datagen = ImageDataGenerator(rescale=1./255)  # 将所有图像乘以 1/255 缩放
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')  # 因为使用了binary_crossentropy损失,所以需要用二进制标签
+    validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+    for data_batch, labels_batch in train_generator:
+        print('data batch shape:', data_batch.shape)
+        print('labels batch shape:', labels_batch.shape)
+        break
+
+    # 利用批量生成器拟合模型
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=100,
+        epochs=30,
+        validation_data=validation_generator,
+        validation_steps=50)
+
+    # 保存模型
+    model.save('cats_and_dogs_small_1.h5')
+
+    # 绘制训练过程中的损失曲线和精度曲线
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+if data_improve_example:
+    datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+    fnames = [os.path.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
+    img_path = fnames[3]  # 选择一张图像进行增强
+    img = image.load_img(img_path, target_size=(150, 150))  # 读取图像并调整大小
+    x = image.img_to_array(img)  # 将其转换为形状 (150, 150, 3) 的 Numpy 数组
+    x = x.reshape((1,) + x.shape)  # 将其形状改变为 (1, 150, 150, 3)
+    i = 0
+    for batch in datagen.flow(x, batch_size=1):
+        plt.figure(i)
+        imgplot = plt.imshow(image.array_to_img(batch[0]))
+        i += 1
+        if i % 4 == 0:
+            break
+    plt.show()
+
+if the_second_exp:
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=(150, 150, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizers.RMSprop(lr=1e-4),
+                  metrics=['acc'])
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)  # 注意,不能增强验证数据
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size = (150, 150),
+        batch_size = 32,
+        class_mode = 'binary')
+    validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size = (150, 150),
+        batch_size = 32,
+        class_mode = 'binary')
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=100,
+        epochs=100,
+        validation_data=validation_generator,
+        validation_steps=50)
+    model.save('cats_and_dogs_small_2.h5')
+
+    # 绘制训练过程中的损失曲线和精度曲线
+    plt.clf()
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
