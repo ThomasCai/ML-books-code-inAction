b3ccc4d412a3bf035debc53e2c05ff3707e11998
thomas_cai <295727207@qq.com>
2019-12-17 08:30:32 +0800

    update

--- deep-learning-with-python/ch05/5.3_pretrained_cnn.py
+++ deep-learning-with-python/ch05/5.3_pretrained_cnn.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 
-
 conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
 
 # 不使用数据增强的快速特征提取
@@ -17,8 +16,10 @@ base_dir = '/home/thomas/Downloads/keras_ch05_data/cats_and_dogs_small'
 train_dir = os.path.join(base_dir, 'train')
 validation_dir = os.path.join(base_dir, 'validation')
 test_dir = os.path.join(base_dir, 'test')
-datagen = ImageDataGenerator(rescale=1./255)
+datagen = ImageDataGenerator(rescale=1. / 255)
 batch_size = 20
+
+
 def extract_features(directory, sample_count):
     features = np.zeros(shape=(sample_count, 4, 4, 512))
     labels = np.zeros(shape=(sample_count))
@@ -30,12 +31,14 @@ def extract_features(directory, sample_count):
     i = 0
     for inputs_batch, labels_batch in generator:
         features_batch = conv_base.predict(inputs_batch)
-        features[i * batch_size : (i + 1) * batch_size] = features_batch
-        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        features[i * batch_size: (i + 1) * batch_size] = features_batch
+        labels[i * batch_size: (i + 1) * batch_size] = labels_batch
         i += 1
         if i * batch_size >= sample_count:
             break
     return features, labels
+
+
 train_features, train_labels = extract_features(train_dir, 2000)
 validation_features, validation_labels = extract_features(validation_dir, 1000)
 test_features, test_labels = extract_features(test_dir, 1000)
@@ -82,41 +85,41 @@ print(model.summary())
 
 # 冻结网络
 print('This is the number of trainable weights '
-'before freezing the conv base:', len(model.trainable_weights))
+      'before freezing the conv base:', len(model.trainable_weights))
 conv_base.trainable = False
 print('This is the number of trainable weights '
-'after freezing the conv base:', len(model.trainable_weights))
+      'after freezing the conv base:', len(model.trainable_weights))
 
 # 利用冻结的卷积基端到端地训练模型
 train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest')
-test_datagen = ImageDataGenerator(rescale=1./255)
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        target_size=(150, 150),
-        batch_size=20,
-        class_mode='binary')
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
 validation_generator = test_datagen.flow_from_directory(
-        validation_dir,
-        target_size=(150, 150),
-        batch_size=20,
-        class_mode='binary')
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
 model.compile(loss='binary_crossentropy',
-        optimizer=optimizers.RMSprop(lr=2e-5),
-        metrics=['acc'])
+              optimizer=optimizers.RMSprop(lr=2e-5),
+              metrics=['acc'])
 history = model.fit_generator(
-        train_generator,
-        steps_per_epoch=100,
-        epochs=30,
-        validation_data=validation_generator,
-        validation_steps=50)
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
@@ -148,14 +151,14 @@ for layer in conv_base.layers:
         layer.trainable = False
 
 model.compile(loss='binary_crossentropy',
-            optimizer=optimizers.RMSprop(lr=1e-5),
-            metrics=['acc'])
+              optimizer=optimizers.RMSprop(lr=1e-5),
+              metrics=['acc'])
 history = model.fit_generator(
-            train_generator,
-            steps_per_epoch=100,
-            epochs=100,
-            validation_data=validation_generator,
-            validation_steps=50)
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=50)
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
@@ -183,6 +186,8 @@ def smooth_curve(points, factor=0.8):
         else:
             smoothed_points.append(point)
     return smoothed_points
+
+
 plt.plot(epochs, smooth_curve(acc), 'bo', label='Smoothed training acc')
 
 plt.plot(epochs, smooth_curve(val_acc), 'b', label='Smoothed validation acc')
@@ -196,9 +201,9 @@ plt.legend()
 plt.show()
 
 test_generator = test_datagen.flow_from_directory(
-        test_dir,
-        target_size=(150, 150),
-        batch_size=20,
-        class_mode='binary')
+    test_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
 test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
 print('test acc:', test_acc)
